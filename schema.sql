5d64801c73ab432a86a313e0a5024563c682c747
nnutley <nnutley@arizona.edu>
2023-01-17 11:18:01 -0800

    corrected ERD diagram & titles table code

--- schema.sql
+++ schema.sql
@@ -47,11 +47,11 @@ CREATE TABLE dept_emp (
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
