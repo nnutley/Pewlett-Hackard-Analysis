7467d2d21984f0c2fe0033479f669dd92efce062
nnutley <nnutley@arizona.edu>
2023-01-24 13:55:51 -0800

    completed README.md and additional tables for summary

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -17,7 +17,7 @@ SELECT DISTINCT ON (emp_no) rt.emp_no,
 	rt.first_name,
 	rt.last_name,
 	rt.title
-INTO unique_titles
+--INTO unique_titles
 FROM retirement_titles as rt
 WHERE (rt.to_date= '1999-01-01')
 ORDER BY rt.emp_no, rt.to_date DESC;
@@ -25,7 +25,7 @@ ORDER BY rt.emp_no, rt.to_date DESC;
 --Create retiring titles table
 SELECT COUNT(title), 
 	ut.title
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY ut.count DESC;
@@ -38,7 +38,7 @@ SELECT DISTINCT ON (emp_no) e.emp_no,
 		de.from_date,
 		de.to_date,
 		ti.title
-INTO mentorship_eligibility
+--INTO mentorship_eligibility
 FROM employees as e
 INNER JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
@@ -47,3 +47,68 @@ ON (e.emp_no = ti.emp_no)
 WHERE (de.to_date = '1999-01-01')
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+
+
+--Joining the Employees, titles, & department info. tables
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date,
+		di.dept_name
+--INTO retirement_dept
+FROM employees as e
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+INNER JOIN dept_info as di
+ON e.emp_no = di.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) rd.emp_no,
+	rd.first_name,
+	rd.last_name,
+	rd.title,
+	rd.dept_name
+--INTO unique_dept
+FROM retirement_dept as rd
+WHERE (rd.to_date= '1999-01-01')
+ORDER BY rd.emp_no, rd.to_date DESC;
+
+--Create retiring departments table
+SELECT COUNT(dept_name), 
+	ud.dept_name
+--INTO retiring_dept
+FROM unique_dept as ud
+GROUP BY dept_name
+ORDER BY ud.count DESC;
+
+--Create mentorship-dept table
+SELECT DISTINCT ON (emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		de.dept_no,
+		d.dept_name
+--INTO mentorship_dept
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE (de.to_date = '1999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+--Create mentorship departments 2 table
+SELECT COUNT(dept_name), 
+	md.dept_name
+--INTO mentorship_dept_2
+FROM mentorship_dept as md
+GROUP BY dept_name
+ORDER BY md.count DESC;
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Pewlett-Hackard-Analysis
+# **Pewlett Hackard Analysis**
+
+## *Overview of Analysis:*
+    -The purpose of this analysis was to use SQL-based data modeling and engineering to address a number of questions that ultimately determined which employees within in the Pewett Hackward data were eligibile for a retirement package and which positions would need to be filled in the near future.
+
+## *Results:*
+    -From the first analysis, it was determined that there were 72,458 current employees that are within the predetermined retirement age.
+    -In this first analysis, it was also determined that the majority of these employees were either Senior Engineers or Senior Staff, making up 35.8% and 34.4% (respectively) of the total number of employees within the predetermined retirement age.
+    -From the second analysis, it was detemined that there were 1,549 current employees that are eligible to be part of a mentirship program for in-coming employees.
+    -In this second analysis, it was also determined that the majority of these employees were either Senior Staff or Engineers, making up 27.1% and 26.0% (respectively) of the total number of employees eligible to be a part of a mentorship program.
+
+![Retiring Titles Table](/Tables/retiring_titles.png)
+
+![Mentorship Titles Table](/Tables/mentorship_titles.png)
+
+## *Summary:*
+    -In order to determine how many roles will need to be filled as the "silver tsunami" begins to make an impact, there may need to be additional analysis done to determine which departments will be impacted the most. An example of a table that may provide more insight is shown below:
+
+![Retiring Departments Table](/Tables/retiring_dept.png)
+
+    -In order to determine if there are enough qualified, retirement-ready employees in the departments to mentor the next generation of employees, there also must be an additional analysis done to determine which departments each of the mentorship eligible employees are in. An example of a table that may provide more insight is shown below:
+    
+![Mentorship Departments Table](/Tables/mentorship_dept.png)
